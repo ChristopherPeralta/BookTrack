7e40e6f4e49c9dd87d6314a917ae006ea07cc0fd
Christopher Peralta <114698519+ChristopherPeralta@users.noreply.github.com>
2024-03-07 07:52:44 -0500

    init

---
+++ BookTracker/BookTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTrackerApp.swift
+//  BookTracker
+//
+//  Created by Christopher Peralta on 7/03/24.
+//
+
+import SwiftUI
+
+@main
+struct BookTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookTracker
+//
+//  Created by Christopher Peralta on 7/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
