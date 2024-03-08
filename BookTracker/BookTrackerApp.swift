7d54066fb79b37e01c17b63f5076b1da7a77a54d
Christopher Peralta <114698519+ChristopherPeralta@users.noreply.github.com>
2024-03-08 08:36:31 -0500

    App
    Add class Book with SwiftData

--- BookTracker/BookTrackerApp.swift
+++ BookTracker/BookTrackerApp.swift
@@ -5,13 +5,15 @@
 //  Created by Christopher Peralta on 7/03/24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
-struct BookTrackerApp: App {
+    struct BookTrackerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Book.self)
     }
 }
